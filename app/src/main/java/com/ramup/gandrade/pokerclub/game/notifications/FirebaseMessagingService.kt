4fd752dd4dd286f558752534021e8ff725f1aea0
German Andrade <German.Andrade@endava.com>
2018-09-12 08:18:46 -0500

    Refactor, saved strings as resources, removed harcoded and replaced with constants

--- app/src/main/java/com/ramup/gandrade/pokerclub/game/notifications/FirebaseMessagingService.kt
+++ app/src/main/java/com/ramup/gandrade/pokerclub/game/notifications/FirebaseMessagingService.kt
@@ -2,6 +2,8 @@ package com.ramup.gandrade.pokerclub.game.notifications
 
 import ACTION_ACCEPT_TRANSACTION
 import ACTION_REJECT_TRANSACTION
+import DATA
+import ID
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
@@ -10,15 +12,16 @@ import android.media.RingtoneManager
 import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.ramup.gandrade.pokerclub.game.NotificationCounter
 import com.ramup.gandrade.pokerclub.R
+import com.ramup.gandrade.pokerclub.game.NotificationCounter
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
 
+const private val CHANNEL = "Channel"
+
 class FirebaseMessagingService() : FirebaseMessagingService(), KoinComponent {
 
-    val TAG = "Service"
-    private val CHANNEL = "Channel"
+    private val TAG = FirebaseMessagingService::class.simpleName
     val notificationCounter by inject<NotificationCounter>()
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
@@ -34,7 +37,7 @@ class FirebaseMessagingService() : FirebaseMessagingService(), KoinComponent {
     private fun sendUserNotification(data: Data) {
         val defaultSoundUri = RingtoneManager.getDefaultUri((RingtoneManager.TYPE_NOTIFICATION))
         val notificationBuilder = NotificationCompat.Builder(applicationContext, CHANNEL)
-                .setContentText(message(data, data.success!!))
+                .setContentText(message(data, requireNotNull(data.success) { "data.success was null at $TAG" }))
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setSound(defaultSoundUri)
@@ -48,20 +51,20 @@ class FirebaseMessagingService() : FirebaseMessagingService(), KoinComponent {
     private fun sendAdminNotification(data: Data) {
         val currentNotificationId = notificationCounter.getId()
         val acceptIntent = Intent(ACTION_ACCEPT_TRANSACTION)
-        acceptIntent.putExtra("data", data)
-        acceptIntent.putExtra("id", currentNotificationId)
+        acceptIntent.putExtra(DATA, data)
+        acceptIntent.putExtra(ID, currentNotificationId)
         val acceptPendingIntent = PendingIntent.getBroadcast(this, currentNotificationId, acceptIntent, PendingIntent.FLAG_ONE_SHOT)
 
         val rejectIntent = Intent(ACTION_REJECT_TRANSACTION)
-        rejectIntent.putExtra("data", data)
-        rejectIntent.putExtra("id", currentNotificationId)
+        rejectIntent.putExtra(DATA, data)
+        rejectIntent.putExtra(ID, currentNotificationId)
         val rejectPendingIntent = PendingIntent.getBroadcast(this, currentNotificationId, rejectIntent, PendingIntent.FLAG_ONE_SHOT)
 
         val defaultSoundUri = RingtoneManager.getDefaultUri((RingtoneManager.TYPE_NOTIFICATION))
         val notificationBuilder = NotificationCompat.Builder(applicationContext, CHANNEL).setContentText(data.toString())
                 .setAutoCancel(true)
-                .addAction(R.drawable.ic_accept, "Accept", acceptPendingIntent)
-                .addAction(R.drawable.ic_reject, "Reject", rejectPendingIntent)
+                .addAction(R.drawable.ic_accept, getString(R.string.accept), acceptPendingIntent)
+                .addAction(R.drawable.ic_reject, getString(R.string.reject), rejectPendingIntent)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setSound(defaultSoundUri)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
@@ -72,7 +75,7 @@ class FirebaseMessagingService() : FirebaseMessagingService(), KoinComponent {
     }
 
     fun message(data: Data, success: Boolean): String {
-        return "${if (success) "Succesfully" else "Failed"} ${data.requestType} ${if (data.extra != null) data.extra else ""}"
+        return "${if (success) getString(R.string.succesfully) else getString(R.string.failed)} ${data.requestType} ${if (data.extra != null) data.extra else ""}"
     }
 
 
